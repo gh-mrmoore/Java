5a68ee179986b2280db0772eddc2def571e10077
mrm <matt.rmoore@gmail.com>
2019-05-13 16:59:40 -0500

    Start of Unit 3 prep work

---
+++ LC101u3/src/main/java/pro/matthewmoore/unit3gradle/Unit3GradleApplication.java
@@ -0,0 +1,13 @@
+package pro.matthewmoore.unit3gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit3GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit3GradleApplication.class, args);
+	}
+
+}
---
+++ LC101u3/src/test/java/pro/matthewmoore/unit3gradle/Unit3GradleApplicationTests.java
@@ -0,0 +1,16 @@
+package pro.matthewmoore.unit3gradle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Unit3GradleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
